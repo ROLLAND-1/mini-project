bea4f9da428202611be600c77862ed72d5a38c41
GHARTEY11 <cobbyevans77@gmail.com>
2021-07-21 14:51:43 +0000

    seat reservation

--- views/script.js
+++ views/script.js
@@ -69,46 +69,63 @@ videoBtn.forEach(btn =>{
     });
 });
 
-var swiper = new Swiper(".review-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        640: {
-          slidesPerView: 1,
-        },
-        768: {
-          slidesPerView: 2,
-        },
-        1024: {
-          slidesPerView: 3,
-        },
-    },
-});
+// var swiper = new Swiper(".review-slider", {
+//     spaceBetween: 20,
+//     loop:true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//         640: {
+//           slidesPerView: 1,
+//         },
+//         768: {
+//           slidesPerView: 2,
+//         },
+//         1024: {
+//           slidesPerView: 3,
+//         },
+//     },
+// });
 
-var swiper = new Swiper(".brand-slider", {
-    spaceBetween: 20,
-    loop:true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-        450: {
-          slidesPerView: 2,
-        },
-        768: {
-          slidesPerView: 3,
-        },
-        991: {
-          slidesPerView: 4,
-        },
-        1200: {
-          slidesPerView: 5,
-        },
-      },
-});
+// var swiper = new Swiper(".brand-slider", {
+//     spaceBetween: 20,
+//     loop:true,
+//     autoplay: {
+//         delay: 2500,
+//         disableOnInteraction: false,
+//     },
+//     breakpoints: {
+//         450: {
+//           slidesPerView: 2,
+//         },
+//         768: {
+//           slidesPerView: 3,
+//         },
+//         991: {
+//           slidesPerView: 4,
+//         },
+//         1200: {
+//           slidesPerView: 5,
+//         },
+//       },
+// });
  
+const container = document.querySelector('.container');
+const seat = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const busSelect = document.getElementById('bus')
+
+let ticketprice = +busSelect.value;
+
+
+
+
+container.addEventListener('click',(e) => {
+  if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+    // console.log(e.target);
+    e.target.classList.toggle('selected');
+  }
+});
